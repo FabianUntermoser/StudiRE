058c61621f3f7414a8c5730c1976e3ba7a470407
Fab <f.untermoser@gmx.at>
2018-04-25 16:13:37 +0200

    [REFACTOR] Add TODOs

--- src/main/java/presentation/controller/IndexController.java
+++ src/main/java/presentation/controller/IndexController.java
@@ -6,6 +6,10 @@ import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import java.io.Serializable;
 
+// TODO: Provide Page for searching students by name
+// TODO: Provide Login Page
+// TODO: Use SSL encryption
+
 @Named
 @ViewScoped
 public class IndexController implements Serializable {
