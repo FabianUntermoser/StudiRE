b688eaf7e2eba6f5edd2aa649f52599d62a6a5fb
Fab <f.untermoser@gmx.at>
2018-05-02 14:01:45 +0200

    [TASK] Replace BASIC Authentication with login page

--- src/main/java/presentation/controller/IndexController.java
+++ src/main/java/presentation/controller/IndexController.java
@@ -6,8 +6,6 @@ import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import java.io.Serializable;
 
-// TODO: Provide Login Page
-
 @Named
 @ViewScoped
 public class IndexController implements Serializable {
